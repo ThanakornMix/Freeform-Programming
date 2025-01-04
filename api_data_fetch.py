830e45c026f69e8081dd21c2780f29da35a5f08b
ThanakornMix <150530415+ThanakornMix@users.noreply.github.com>
2025-01-04 04:01:27 +0000

    modified:   api_data_fetch.py

--- api_data_fetch.py
+++ api_data_fetch.py
@@ -1,22 +1,21 @@
 import requests
 import pandas as pd
 
-
+# Define the base URL and your API Key
 URL_BASE = "https://api.eia.gov/v2/"
-API_KEY = "jhkTkcgtidrXrRFPGYXtgug0ZCygKtgvzS4LwDw2"  
+API_KEY = "jhkTkcgtidrXrRFPGYXtgug0ZCygKtgvzS4LwDw2"  # Replace with your actual API key
 
-
+# Function to fetch data from a specific API route
 def fetch_data_from_api(route):
-    url = f"{URL_BASE}{route}/data"  
+    url = f"{URL_BASE}{route}/data"  # Construct the full URL with the "data" suffix
     params = {'api_key': API_KEY}
     
-  
+    # Send GET request to fetch data from the API
     res = requests.get(url, params=params)
     
-
+    # Check if the request was successful
     if res.status_code == 200:
-        return res.json()  
+        return res.json()  # Return the response data in JSON format
     else:
         print(f"Failed to fetch data. HTTP Status Code: {res.status_code}")
         return None
-
